715efa6b0a8bfc3e9ce9947e7650bc0ddf879937
Phillip Rhodes <motley.crue.fan@gmail.com>
2011-08-13 22:06:26 -0400

    adds retrieving and parsing RSS feed with Rome from Feeds plugin

--- grails-app/controllers/org/trijug/HomeController.groovy
+++ grails-app/controllers/org/trijug/HomeController.groovy
@@ -1,13 +1,62 @@
 package org.trijug
 
+import com.sun.syndication.feed.synd.SyndEntry
+import com.sun.syndication.feed.synd.SyndFeed
+import com.sun.syndication.io.SyndFeedInput
+import com.sun.syndication.io.XmlReader
+
 class HomeController {
 
 	def index = {
 	
 		List<Entry> allEntries = new ArrayList<Entry>();
+		List<RssFeed> feeds = RssFeed.list();
 		
-		Entry entry = new Entry( title: "Whatever", url: "http://www.example.com" );
-		allEntries.add( entry );
+		for( RssFeed rssFeed in feeds )
+		{
+			// lookup the feed, and get the FeedUrl
+			String url = rssFeed.feedUrl;
+			println "Loading from url: ${url}";
+
+			
+			// load the feed, and create an Entry for each link in the RssFeed
+			URL feedUrl = new URL(url);
+			SyndFeedInput input = new SyndFeedInput();
+			SyndFeed feed = null;
+			XmlReader reader = null;
+			try
+			{
+				reader = new XmlReader(feedUrl)
+				feed = input.build(reader);
+				println "Feed: ${feed.getTitle()}"
+				
+				List<SyndEntry> entries = feed.getEntries();
+				
+				for( SyndEntry entry in entries )
+				{
+					String linkUrl = entry.getLink();
+					String linkTitle = entry.getTitle();
+										
+					println "creating and adding entry for link: ${linkUrl} with title: ${linkTitle}"
+				
+					Entry anEntry = new Entry( title: linkTitle, url: linkUrl );
+					allEntries.add( anEntry );	
+				}
+			}
+			catch( Exception e )
+			{
+				e.printStackTrace();
+			}
+			finally
+			{
+				if( reader != null )
+				{
+					reader.close();
+				}
+			}
+
+			
+		}
 		
 		[allEntries: allEntries];	
 	}
