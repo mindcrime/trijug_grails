3c3def88953e0255658ae3a6dc5cd20761aed95b
Phillip Rhodes <motley.crue.fan@gmail.com>
2011-08-13 22:51:06 -0400

    uses the BootStrap to add some Feeds at startup time

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,71 @@
+import grails.util.Environment
+
+import org.trijug.RssFeed
+
 class BootStrap {
 
     def init = { servletContext ->
-    }
-    def destroy = {
-    }
+    
+		switch( Environment.current )
+		{
+			case Environment.DEVELOPMENT:
+				createSomeFeeds();
+				break;
+			case Environment.PRODUCTION:
+				println "No special configuration required";
+				break;
+		}
+			
+	}
+    
+	def destroy = {
+    
+		
+	}
+	
+	private void createSomeFeeds()
+	{
+		RssFeed feedOne = new RssFeed( feedUrl: 'http://feeds.dashes.com/AnilDash' );
+		if( feedOne.save() )
+		{
+			println( "created feedOne" );
+		}
+		else 
+		{
+			println( "failed to create feedOne" );
+		}	
+		
+		RssFeed feedTwo = new RssFeed( feedUrl: 'http://www.kmworld.com/rss/default.aspx?Criteria=popularity&days=10&rows=10' );
+		if( feedTwo.save() )
+		{
+			println( "created feedTwo" );
+		}
+		else 
+		{
+			println( "failed to create feedTwo" );
+		}	
+
+		RssFeed feedThree = new RssFeed( feedUrl: 'http://www.jroller.com/rss' );
+		if( feedThree.save() )
+		{
+			println( "created feedThree" );
+		}
+		else
+		{
+			println( "failed to create feedThree" );
+		}
+
+		RssFeed feedFour = new RssFeed( feedUrl: 'http://www.linuxjournal.com/news.rss' );
+		if( feedFour.save() )
+		{
+			println( "created feedFour" );
+		}
+		else
+		{
+			println( "failed to create feedFour" );
+		}
+
+		
+	}
+	
 }
