7c5809e77c7dfccd116fc5dc71a0b0b24568754c
Phillip Rhodes <motley.crue.fan@gmail.com>
2011-08-14 02:30:31 -0400

    adds support for producing an RSS feed

--- grails-app/controllers/org/trijug/HomeController.groovy
+++ grails-app/controllers/org/trijug/HomeController.groovy
@@ -52,4 +52,38 @@ class HomeController {
 		
 	}
 	
+	def renderRss = {
+
+		List<Entry> allEntries = new ArrayList<Entry>();
+		def page = params.page;
+		if( !page )
+		{
+			page = 1;
+		}
+		else
+		{
+			page = Integer.parseInt( page );
+		}
+				
+		// get the entries via query now...
+		List<Entry> queryResults =
+		Entry.executeQuery( "select entry from Entry as entry order by entry.dateCreated desc",
+			[], [max: page * 10 ]);
+		
+		render(feedType:"rss", feedVersion:"2.0") {
+			title = "Tri-JUG Grails Feed"
+			link = "$http://localhost:8080/"
+			description = "Feed for Tri-JUG Grails Project"
+
+			queryResults.each() {
+				article -> entry(article.title)
+				{
+					link = article.url;
+					publishedDate = article.dateCreated;
+					"Content"
+				}
+			}
+		}
+	}
+	
 }
